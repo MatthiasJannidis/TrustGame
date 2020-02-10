1704930125ef40f281b90e6665f5361845a9cbcf
Matthias Jannidis <matthias.jannidis@gmail.com>
2020-02-10 14:27:16 +0100

    enemy is a prefab now

--- TrustGame/Assets/Scripts/Spawner.cs
+++ TrustGame/Assets/Scripts/Spawner.cs
@@ -7,11 +7,16 @@ public class Spawner : MonoBehaviour
     [SerializeField, Range(1, 10)] int amount;
     [SerializeField] GameObject spawnedPrefab;
     [SerializeField] Transform[] spawnPositions;
+    [SerializeField] AudioClip spawningClip;
+
+    [SerializeField] AudioSource audioSource;
 
     public void Trigger() 
     {
+        audioSource.PlayOneShot(spawningClip);
         for(int i = 0; i < amount; i++) 
         {
+            
             if (spawnPositions.Length == 1) Instantiate(spawnedPrefab, spawnPositions[0].position, Quaternion.identity);
             else Instantiate(spawnedPrefab, spawnPositions[i].position, Quaternion.identity);
         }
